043df5e8f5305eae6459862b0806afa9ce6a2f14
Hallvard <hallvarbjordalsbakke@hotmail.com>
2017-11-11 13:50:44 +0100

    Added PsotValuesToDatabase. Added sendValues in Character Sheet (not yet functional).

--- app/src/main/java/no/ntnu/hallvardpc/loadeddwarvendice/CharacterSheetActivity.java
+++ app/src/main/java/no/ntnu/hallvardpc/loadeddwarvendice/CharacterSheetActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.net.MalformedURLException;
@@ -33,7 +34,10 @@ public class CharacterSheetActivity extends AppCompatActivity {
         }   catch (MalformedURLException e)
         {
             e.printStackTrace();
-        }
+    }
+        //Believe we need an array with all the names of the EditText we want to listen to, and then run a while loop for them.
+        EditText targetEditText = (EditText)findViewById(R.id.target);
+        targetEditText.setOnEditorActionListener(sendValue(target));
     }
 
 
@@ -51,5 +55,18 @@ public class CharacterSheetActivity extends AppCompatActivity {
 
         }
     }
+
+    public void sendValue(View view) {
+        TextView t = (TextView) findViewById(R.id.target);
+        String text = t.getText().toString();
+        System.out.println("In sendValue " + text);
+        new PostValueToDatabase() {
+            @Override
+            protected void onPostExecute(Boolean status) {
+                System.out.println("Got on post value " + status);
+            }
+        }.execute(new PostValueToDatabase.PostValue("http://158.38.101.111/characterSheets/0000000000000000" ,text));
+    }
+
 
 }
---
+++ app/src/main/java/no/ntnu/hallvardpc/loadeddwarvendice/PostValueToDatabase.java
@@ -0,0 +1,100 @@
+package no.ntnu.hallvardpc.loadeddwarvendice;
+
+import android.os.AsyncTask;
+import android.util.JsonWriter;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Hallvard on 11.11.2017.
+ */
+
+public class PostValueToDatabase extends AsyncTask<PostValueToDatabase.PostValue,Void,Boolean> {
+    public PostValueToDatabase() {
+        super();
+    }
+
+    /**
+     * This method will run as a background thread
+     *
+     * @param values
+     * @return
+     */
+    @Override
+    protected Boolean doInBackground(PostValue... values) {
+        for (PostValue value : values) {
+            try {
+                // Setup request
+                URL url = new URL(value.getUrl());
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setDoOutput(true);    // We are going to write to the server
+                con.setUseCaches(false);  // Not interested in browser/http cache
+                con.setRequestMethod("POST"); // Using POST command
+                con.setRequestProperty("Cache-Control", "no-cache");
+                con.setRequestProperty("Connection", "Keep-Alive");
+                con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");  // JSON
+                //        con.connect();
+                // Write Message object
+                BufferedWriter bw = new BufferedWriter(
+                        new OutputStreamWriter(con.getOutputStream(),"UTF-8"));
+                JsonWriter jw = new JsonWriter(bw); // Android JSON support library
+                jw.beginObject()
+                        .name("text")
+                        .value(value.getText())
+                        .endObject()
+                        .close();
+
+                // Get response from server
+                StringBuilder result = new StringBuilder();
+                int len;
+                if(con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    Reader br = new InputStreamReader(new BufferedInputStream(con.getInputStream()),"UTF-8");
+                    char[] cbuff = new char[1024];
+                    while((len = br.read(cbuff)) != -1) {
+                        result.append(cbuff,0,len);
+                    }
+                    br.close();
+                } else {
+                    Log.e("PostValue", "ResponseCode: " + con.getResponseCode());
+                }
+                con.disconnect();
+            } catch (IOException e) {
+                Log.e("PostValue", "doInBackground: ", e);
+            }
+        }
+
+        return true;
+    }
+
+
+
+
+
+    public static class PostValue {
+        String url;
+        String text;
+
+        public PostValue(String url, String text) {
+            this.url = url;
+            this.text = text;
+
+
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public String getText() {
+            return text;
+        }
+    }
+}
