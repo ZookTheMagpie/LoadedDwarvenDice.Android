ca4bd06d27a463e02a691c4b06e960d334c0efa2
Alexander <zookthemagpie@hotmail.com>
2017-11-29 14:50:52 +0100

    More dice work

---
+++ app/src/main/java/no/ntnu/hallvardpc/loadeddwarvendice/ImageAdapter.java
@@ -0,0 +1,62 @@
+package no.ntnu.hallvardpc.loadeddwarvendice;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import java.util.List;
+
+/**
+ * Created by Alexander Eilert Berg on 29/11/2017.
+ */
+
+class ImageAdapter extends BaseAdapter
+{
+    private Context context;
+    private int numberOfDice = 0;
+
+    private List<Integer> diceIDs;
+
+
+    public ImageAdapter(Context context, int numberOfDice, List<Integer> diceIDs)
+    {
+        this.context = context;
+        this.numberOfDice = numberOfDice;
+        this.diceIDs = diceIDs;
+    }
+
+    @Override
+    public int getCount() {
+        return numberOfDice;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup)
+    {
+        ImageView diceImage = new ImageView(context);
+        diceImage.setPadding(10,10,10,10);
+
+        diceImage.setAdjustViewBounds(true);
+        diceImage.setMaxHeight(256);
+        diceImage.setMaxWidth(256);
+        diceImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+        System.out.println("A: " + diceIDs);
+
+        diceImage.setImageResource(diceIDs.get(i));
+        return diceImage;
+    }
+
+}
